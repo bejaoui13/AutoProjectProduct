b75ec1e515313af6c7fecd5605bc9f055ad38044
islem <ssafa197@gmail.com>
2023-11-23 23:16:56 +0000

    update commit search

--- src/test/java/com/tutorial/testcase/SearchTest.java
+++ src/test/java/com/tutorial/testcase/SearchTest.java
@@ -2,6 +2,10 @@ package com.tutorial.testcase;
 
 import java.util.concurrent.TimeUnit;
 
+
+
+
+
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
@@ -11,7 +15,9 @@ import org.testng.annotations.Test;
 import com.tutorial.page.HomePage;
 import com.tutorial.page.SearchPage;
 import com.tutoriall.base.Base;
- 
+
+
+ //commit
 
 public class SearchTest extends Base {
 	
